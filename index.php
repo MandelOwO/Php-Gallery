8ce3e60064639f885a6fbe657583900bee007965
SSSVT-06 <dvorakmichal@sssvt.cz>
2022-12-14 13:30:13 +0100

    +sites

--- index.php
+++ index.php
@@ -1,1 +1,19 @@
 <?php
+define('APP_DIR',  $_SERVER['DOCUMENT_ROOT'].'/09_gallery/Php-Gallery/');
+
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+</body>
+</html>
