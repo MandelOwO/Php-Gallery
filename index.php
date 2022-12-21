5b66a6789fa9fee61aae7c81c1c850b5d2c2c55d
sssvt-dvorak-michal <dvorakmichal@sssvt.cz>
2022-12-21 12:30:33 +0100

    Home

--- index.php
+++ index.php
@@ -11,19 +11,35 @@ define('APP_DIR', $_SERVER['DOCUMENT_ROOT'] . '/09_gallery/Php-Gallery/');
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <link rel="stylesheet" href="styles/style.css">
-    <title>kocky.cz | domů</title>
+    <title>Dexlery | Home</title>
 </head>
 <body id="home">
 
 <header>
-    <h1><a href="<?= APP_DIR ?>index.php">Kočky.cz</a></h1>
+    <h1>
+        <a href="<?= APP_DIR ?>index.php">
+            <i class="nes-pokeball"></i>
+            <span class="nes-text pokebal-red">Dexlery</span>
+        </a>
+    </h1>
 </header>
 
 <main>
-    <div class="nes-container with-title is-rounded">
-        <p class="title">Nejnovější galerie</p>
-        <p>Good morning. Thou hast had a good night's sleep, I hope.</p>
-    </div>
+    <section class="nes-container is-rounded">
+        <div class="gallery-grid">
+
+            <div class="nes-container with-title is-centered">
+                <p class="title">Container</p>
+                <p>Good morning. Thou hast had a good night's sleep, I hope.</p>
+            </div>
+
+        </div>
+    </section>
+
+    <section class="new-gallery-dialog">
+        <a class="nes-btn is-primary" href="new-gallery.php">Vytvořit novou galerii</a>
+    </section>
+
 </main>
 
 
