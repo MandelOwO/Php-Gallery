508cbd1c8622f4ca93dededce978bf26e0461d68
MandelOwO <87827886+MandelOwO@users.noreply.github.com>
2022-12-27 14:41:52 +0100

    Image upload script

--- new-gallery.php
+++ new-gallery.php
@@ -12,16 +12,20 @@ define('APP_DIR', $_SERVER['DOCUMENT_ROOT'] . '/09_gallery/Php-Gallery/');
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
     <link rel="stylesheet" href="styles/style.css">
-    <script src="node_modules/remove-focus-outline/outline.js"></script>
+    <script src="js/outline.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+
 
     <title>Add gallery</title>
+
+
 </head>
 <body>
 
-<?php require_once 'source/header.php'; ?>
+<?php require_once  APP_DIR.'source/header.php'; ?>
 
 <main>
-    <form class="nes-container is-rounded form">
+    <form method="post" class="nes-container is-rounded form">
         <div class="nes-field">
             <label for="name">Název</label>
             <input type="text" name="name" id="name" class="nes-input">
@@ -33,14 +37,19 @@ define('APP_DIR', $_SERVER['DOCUMENT_ROOT'] . '/09_gallery/Php-Gallery/');
         </div>
 
         <div class="nes-field">
-            <label for="thumbnail">Náhledová fotka</label>
-            <label class="nes-btn">
-                <span>Select your file</span>
-                <input type="file" id="thumbnail">
+            <label for="thumbnail">Náhledová fotka <small>(nepovinné)</small></label>
+            <label for="file-upload" class="nes-btn">
+                <i class="fa fa-cloud-upload"></i> Vyberte obrázek
             </label>
+            <input id="file-upload" name='upload_cont_img' type="file" style="display:none;">
+
+            <script src="js/FileUpload.js"></script>
         </div>
 
-        <!--        TODO https://stackoverflow.com/questions/34841434/display-selected-file-in-html-input-before-upload-->
+        <div>
+            <input type="submit" class="nes-btn is-success" value="Uložit" >
+        </div>
+
     </form>
 </main>
 
